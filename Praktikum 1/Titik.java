8263c50d17a462a4433b0d8fd605043f99ea5895
hanashabrina <100180248+hanashabrina@users.noreply.github.com>
2023-02-23 21:46:39 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,38 @@
+class Titik {
+	
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	Titik(){
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}
+		
+	Titik(double x, double y){
+		absis = x;
+		ordinat = y;
+		counterTitik++;
+	}
+		
+	void setAbsis(double a){
+		absis = a;
+	}
+		
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+		
+	double getAbsis(){
+		return absis;
+	}
+		
+	double getOrdinat(){
+		return ordinat;
+	}
+		
+	static int getCounterTitik(){
+		return counterTitik;
+	}
+}
