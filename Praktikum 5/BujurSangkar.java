cbebab2582a0554372d69dd31b8881da88de3ee5
hanashabrina <100180248+hanashabrina@users.noreply.github.com>
2023-03-29 09:30:58 +0700

    Update BujurSangkar.java

--- Praktikum 5/BujurSangkar.java
+++ Praktikum 5/BujurSangkar.java
@@ -6,6 +6,10 @@
 */
 
 public class BujurSangkar extends BangunDatar{
+	//Jika kelas BujurSangkar tidak membuat implementasi metode abstrak yang ada pada kelas BangunDatar 
+	//maka kelas BujurSangkar juga harus dideklarasikan sebagai kelas abstrak. 
+	//Ini karena, kelas BujurSangkar akan mewarisi metode abstrak dari kelas induknya (BangunDatar) 
+	//tetapi tidak memberikan implementasi untuk metode abstrak tersebut.
 	public double hitungLuas(double sisi){
 		luas = sisi*sisi;
 		return luas;
