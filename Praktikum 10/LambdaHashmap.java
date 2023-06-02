98a864cea821412f37225d5eb08706ffa492629e
hanashabrina <100180248+hanashabrina@users.noreply.github.com>
2023-06-02 21:09:16 +0700

    Add files via upload

---
+++ Praktikum 10/LambdaHashmap.java
@@ -0,0 +1,19 @@
+/**
+* File		: LambdaList.java 31/05/2023
+* Penulis	: Hana Shabrina/24060121130077
+* Deskripsi	: Implementasi lambda pada hash map
+* Lab		: B1
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LambdaHashmap {
+    public static void main(String[] args) {
+        Map<String, String> mahasiswaMap = new HashMap<>();
+        mahasiswaMap.put("24060121130077", "Hana");
+        mahasiswaMap.put("24060121130069", "Ririn");
+
+        mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " +nim+ ", Nama: " +nama));
+    }
+}
